7c3b6047a0083314aa45671aa65f3e606ff5150a
YuziSumoto <yuzi@me.com>
2018-07-30 14:45:10 +0900

    表示ロジックテンプレートへ

--- Ninchi010.py
+++ Ninchi010.py
@@ -25,23 +25,18 @@ class MainHandler(webapp2.RequestHandler):
     if MstUser().ChkUser(user.email()) == False:
       self.redirect(users.create_logout_url(self.request.uri))
       return
-
-    Rec = {} # 画面受け渡し用領域
-
     if self.request.get('Hizuke') == "": # パラメタ無し？
-      Rec["TxtHizuke"] = self.request.cookies.get('Hizuke', '') # Cookieより
+      Hizuke = self.request.cookies.get('Hizuke', '') # Cookieより
     else:
-      Rec["TxtHizuke"] = self.request.get('Hizuke')   # パラメタ取得
+      Hizuke = self.request.get('Hizuke')   # パラメタ取得
       cookieStr = 'Hizuke=' + self.request.get('Hizuke') + ';'     # Cookie保存
       self.response.headers.add_header('Set-Cookie', cookieStr.encode('shift-jis'))
 
     LblMsg = "記録を選択するか、新規作成を押してください"
 
-    strTable  =  self.TableSet(Rec["TxtHizuke"])
-
     template_values = {
-      'Rec'     :Rec,
-      'StrTable':strTable,
+      'Hizuke':Hizuke,
+      'Recs'  :DatSoudan().GetAll(),
       'LblMsg': LblMsg
       }
     path = os.path.join(os.path.dirname(__file__), 'Ninchi010.html')
@@ -63,14 +58,11 @@ class MainHandler(webapp2.RequestHandler):
       self.redirect("/Ninchi020/" + Parm) # 
       return
 
-    strTable  =  self.TableSet(self.request.cookies.get('Hizuke', ''))
-
-    Rec = {} # 画面受け渡し用領域
-    Rec["TxtHizuke"] = self.request.cookies.get('Hizuke', '') # Cookieより
+    Hizuke = self.request.cookies.get('Hizuke', '') # Cookieより
 
     template_values = {
-      'Rec'     :Rec,
-      'StrTable':strTable,
+      'Hizuke':Hizuke,
+      'Recs'  :DatSoudan().GetAll(),
       'LblMsg': LblMsg
       }
     path = os.path.join(os.path.dirname(__file__), 'Ninchi010.html')
@@ -78,53 +70,6 @@ class MainHandler(webapp2.RequestHandler):
 
 ####################################################################################################
 
-#  テーブルセット
-  def TableSet(self,Hizuke):
-
-    retStr = ""
-
-#    Snap = DatSoudan().GetMonthList(Hizuke)
-    Snap = DatSoudan().GetAll()
-
-#    for Ctr in range(1,10):
-    for Rec in Snap:
-
-      retStr += "<TR>"
-
-      retStr += "<TD>"    # 更新ボタン（患者コード)
-      retStr += u"<input type='submit' value = '更新'"
-      retStr += " name='BtnSelect"
-      retStr += str(Rec.key())
-      retStr += "' style='width:80px'>"
-      retStr += "</TD>"
-
-      retStr += "<TD>"    # 患者名
-      retStr += Rec.Hizuke.strftime('%Y/%m/%d')
-      retStr += "</TD>"
-
-      retStr += "<TD>"    # 患者名
-      retStr += Rec.Name
-      retStr += "</TD>"
-
-      retStr += "<TD>"    # 印刷ボタン（患者コード)
-      retStr += u"<input type='button' value = '印刷'"
-      retStr += "onclick='window.open("
-      retStr += '"/Ninchi025/'
-      retStr += "?Key=" + str(Rec.key())
-      retStr += '"'
-      retStr += ");'"
-      retStr += " style='width:50px'>"
-      retStr += "</TD>"
-      retStr += "<TD>"    # 削除ボタン（患者コード)
-      retStr += u"<input type='submit' value = '削除'"
-      retStr += "' name='BtnDel" 
-      retStr += str(Rec.key())
-      retStr += "' style='width:50px'>"
-      retStr += "</TD>"
-
-      retStr += "</TR>"
-
-    return retStr
 
 app = webapp2.WSGIApplication([
     ('/Ninchi010/', MainHandler)
